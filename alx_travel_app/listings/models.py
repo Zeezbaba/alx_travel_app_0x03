785b7572f4adabcb1ac2ad1bbc1118bf7e653d1d
Zeezbaba <okelejitimi93@gmail.com>
2025-05-28 19:23:35 +0100

    Database Modeling and Data Seeding in Django

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -7,6 +7,8 @@ class Listing(models.Model):
     description = models.TextField()
     location = models.CharField(max_length=255)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    available_from = models.DateField(null=True, blank=True)
+    available_to = models.DateField(null=True, blank=True)
     host_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='listings')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -18,8 +20,9 @@ class Booking(models.Model):
     booking_id = models.IntegerField(primary_key=True)
     listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bookings')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
-    start_date = models.DateField(unique='DD-MM-YYYY')
-    end_date = models.DateField(unique='DD-MM-YYYY')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     status = models.CharField(max_length=20, choices=[
         ('pending', 'pending'),
         ('confirmed', 'confirmed'),
