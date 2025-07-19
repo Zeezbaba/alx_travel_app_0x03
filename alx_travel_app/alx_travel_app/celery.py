3daa8616bf9bb4dfd2a1a37089d31075e9e4cc96
Zeezbaba <okelejitimi93@gmail.com>
2025-07-20 00:01:10 +0100

    feat: Added celery

---
+++ alx_travel_app/alx_travel_app/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()  # Loads variables from .env
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+
+app = Celery('alx_travel_app')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
