93ec03e8bd879021c6a07d9603beeefe36fcd1f7
Zeezbaba <okelejitimi93@gmail.com>
2025-07-20 00:40:40 +0100

    feat: Added celery

--- alx_travel_app/alx_travel_app/celery.py
+++ alx_travel_app/alx_travel_app/celery.py
@@ -1,13 +1,32 @@
+# import os
+# from celery import Celery
+# from dotenv import load_dotenv
+
+# load_dotenv()  # Loads variables from .env
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+
+# app = Celery('alx_travel_app')
+
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# app.autodiscover_tasks()
+
+from __future__ import absolute_import
 import os
 from celery import Celery
-from dotenv import load_dotenv
 
-load_dotenv()  # Loads variables from .env
-
+# Set the default Django settings module for 'celery'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
 
 app = Celery('alx_travel_app')
 
+# Load settings from Django settings using CELERY_ prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Automatically discover tasks in installed apps
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
